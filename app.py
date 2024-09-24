03b98e0795031b934f4d8b0a1671f26f200c1997
orlov andrey <andrey3310202@gmail.com>
2024-09-24 20:31:27 +0700

    имя студента

--- app.py
+++ app.py
@@ -341,4 +341,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Орлов Андрей'
+    return render_template('example.html', name=name)
