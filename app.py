74afaffac43946f71ed098e342bea8053ffe94ff
orlov-andrey <andrey3310202@gmail.com>
2024-09-25 18:26:55 +0700

    правки

--- app.py
+++ app.py
@@ -201,6 +201,7 @@ def lab():
             <li><a href="418">Ошибка 418</a></li>
             <li><a href="/about"> заголовки</a></li>
             <li><a href ="/error">ошибка 500</a></li>
+            <li><a href="/lab1/web1"> веб-сервер на flask</a><li>
             </ol>
         <div>
     </body>
@@ -308,51 +309,3 @@ def about():
    
     return response
 
-@app.route('/lab2/a')
-def a():
-    return 'без слэша'
-
-@app.route('/lab2/a/')
-def a1():
-    return 'со слешем'
-
-flower_list = ['роза', 'тюльпан', 'незабудка' , 'ромашка']
-
-@app.route('/lab2/flowers/<int:flower_id>')
-def flowers(flower_id):
-    if flower_id >= len(flower_list):
-        return "такого изящного цветочка нету" , 404
-    else:
-        return "прекрасный цветочек:   " + flower_list[flower_id]
-    
-@app.route('/lab2/add_flower/<name>')
-def add_flower(name):
-    flower_list.append(name)
-    return f'''
-<!doctype html>
-<html>
-    <body>
-    <h1>Добавлен новый цветок</h1>
-    <p>Название нового цветка: {name} </p>
-    <p>Всего цветов: {len(flower_list)}</p>
-    <p>Полный спискок: {flower_list}</p>
-</html>
-'''
-
-@app.route('/lab2/example')
-def example():
-    name, number, groupe, course='Орлов Андрей', 2, 'ФБИ-21', '3 курс'
-    fruits = [
-        {'name': 'яблоки', 'price': 100},
-        {'name': 'груши', 'price': 120},
-        {'name': 'апельсины', 'price': 80},
-        {'name': 'мандарины', 'price': 95}, 
-        {'name': 'манго', 'price': 321},
-    ]
-    return render_template('example.html', name=name, number=number, groupe=groupe, course=course, fruits=fruits)
-
-
-
-@app.route('/lab2/')
-def lab2():
-    return render_template('lab2.html')
