2dcac96c12c11b8962d663749d6b98084c843b4a
orlov-andrey <andrey3310202@gmail.com>
2024-11-04 15:13:09 +0700

    подключение бд к sqlite

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, url_for, redirect, render_template, Response, render_template_string
+from flask import Flask, url_for, redirect, render_template, Response, render_template_string, os
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 app = Flask(__name__)
-app.secret_key = 'секретный ключик'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
