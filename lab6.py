9aff938f1543f55be4c3dfeaa661aeebf406d389
orlov-andrey <andrey3310202@gmail.com>
2024-11-16 15:58:54 +0700

    снятие аренды

--- lab6.py
+++ lab6.py
@@ -47,7 +47,69 @@ def api():
                             'message': 'already booked'
                         },
                         'id': id
+                        }
+                
+
+                # Установить арендатора
+                office['tenant'] = login
+                return {
+                    'jsonrpc': '2.0',
+                    'result': f'Office {office_number} successfully booked',
+                    'id': id
+            }
+
+
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        login = session.get('login')  # Проверка авторизации
+        if not login:
+          return {
+            'jsonrpc': '2.0',
+            'error': {
+                'code': 1,
+                'message': 'Unauthorized'
+            },
+            'id': id
+        }
+
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                        'message': 'Office is not rented'
+                    },
+                    'id': id
                 }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                        'message': 'You cannot cancel someone else\'s reservation'
+                    },
+                    'id': id
+                }
+
+            # Снятие аренды
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': f'Office {office_number} reservation canceled',
+                    'id': id
+            }
+
+        return {
+            'jsonrpc': '2.0',
+            'error': {
+                'code': -32602,
+                'message': 'Invalid office number'
+        },
+        'id': id
+    }
+
 
     return {
         'jsonrpc': '2.0',
