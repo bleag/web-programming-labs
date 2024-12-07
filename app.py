bcb6ca67827165899c8efc75b23eb85070ed4a74
orlov-andrey <andrey3310202@gmail.com>
2024-12-07 17:48:02 +0700

    заготовка лабораторная 7

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
@@ -15,7 +16,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
-
+app.register_blueprint(lab7)
 
 @app.route("/")
 def slesh():
